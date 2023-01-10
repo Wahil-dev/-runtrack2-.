af59e5b656ba46b73d82e287527e873476fe20dc
Wahil-dev <wahil.chettouf@laplatefore.io>
2023-01-10 11:14:21 +0100

    Fin du job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,18 @@
+<?php
+    $str = "LaPlateforme";
+    echo "<br>".$str;
+    $str2 = 'Vive';
+    $str3 = "!";
+
+    echo "<br>".$str2.$str.$str3;
+
+    $val = 6;
+    echo "<br>".$val;
+
+    $val += 4;
+    echo "<br>".$val;
+
+    $myBool = true;
+    echo "<br>".$myBool;
+    $myBool = false;
+    echo "<br>".$myBool;
