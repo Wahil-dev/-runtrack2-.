29e900fc92cebfaa289c867bb4e1322c3d095caf
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-01-20 11:36:40 +0100

    Findu job02 -> jour08

---
+++ jour08/job02/index.php
@@ -0,0 +1,27 @@
+<?php
+    //setcookie(name, value, expire, path, domain, secure, httponly);
+    $cookieName = "nbvisites";
+    $cookieValue = 1;
+    if(isset($_COOKIE[$cookieName])) {
+        echo "<p>$_COOKIE[$cookieName]</p>";
+    } 
+
+    if(!isset($_COOKIE[$cookieName])) {
+        setcookie($cookieName, $cookieValue);
+        header("refresh:0");
+        exit();
+    } else {
+        $cookieValue += $_COOKIE[$cookieName];
+        setcookie($cookieName, $cookieValue);
+        if(isset($_GET['reset'])) {
+            $cookieValue = 0;
+            setcookie($cookieName, $cookieValue, time());
+            header("location: index.php");
+        }
+    }
+?>
+
+<form action="" method="get">
+    <input type="submit" name="reset" value="rest">
+</form>
+
