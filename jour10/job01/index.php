ffd60fe00ab6fbe6422d422f1c66f503fcb064b5
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-01-23 16:46:21 +0100

    Fin de job01 -> jour10

---
+++ jour10/job01/index.php
@@ -0,0 +1,49 @@
+<?php
+    $serverName = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "jour09";
+
+    $conn = new mysqli($serverName, $user, $pass, $dbname);
+    if ($conn->connect_errno) {
+        printf("Connect failed: %s\n", $conn->connect_error);
+        exit();
+    }
+    $request = $conn->query("SELECT prenom, nom, naissance, sexe, email FROM etudiants");
+    $result = $request->fetch_all(MYSQLI_ASSOC);
+    /* check connection */
+?>
+
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="2">
+        <thead>
+            <tr>
+                <?php
+                    foreach($result[0] as $key => $value): ?>
+                        <th><?= $key ?></th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                for($i=1; isset($result[$i]); $i++): ?>
+                    <tr>
+                        <td><?= $result[$i]["prenom"] ?></td>
+                        <td><?= $result[$i]["nom"] ?></td>
+                        <td><?= $result[$i]["naissance"] ?></td>
+                        <td><?= $result[$i]["sexe"] ?></td>
+                        <td><?= $result[$i]["email"] ?></td>
+                    </tr>
+            <?php endfor; ?>
+        </tbody>
+    </table>
+</body>
+</html>
