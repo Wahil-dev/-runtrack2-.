6ec05a39afe3079fb082f89a6ac4a21d2df408cf
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-01-24 15:55:41 +0100

    Fin de job12 -> jour10

---
+++ jour10/job12/index.php
@@ -0,0 +1,48 @@
+<?php
+    $serverName = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "jour09";
+
+    try {
+        $conn = new PDO("mysql:host=$serverName; dbname=$dbname", $user, $pass);
+        $request = $conn->query("SELECT prenom, nom, naissance FROM etudiants WHERE 1998 < LEFT (naissance, 4) && LEFT (naissance, 4) < 2018");
+        $request->execute();
+
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if($request->rowCount()) :?>
+        <table border="4">
+            <thead>
+                <tr>
+                    <th>prenom</th>
+                    <th>nom</th>
+                    <th>naissance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($result = $request->fetchObject()) :?>
+                    <tr>
+                        <td><?= $result->prenom;?></td>
+                        <td><?= $result->nom;?></td>
+                        <td><?= $result->naissance;?></td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    <?php else :?>
+        <h2>Table Vide</h2>
+    <?php endif; ?>
+</body>
+</html>
